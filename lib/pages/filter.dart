85f173096c86ae113947a2e47a5d1b19071722ff
Daniel MS <danielmccannsayles@gmail.com>
2022-02-05 22:03:47 -0800

    adding UI for filter page

--- lib/pages/filter.dart
+++ lib/pages/filter.dart
@@ -1,19 +1,90 @@
 import 'package:flutter/material.dart';
 import '../components/custom_app_bar.dart';
+import 'package:passage_flutter/theme/app_theme.dart';
 
-class FiltersHome extends StatelessWidget {
+import 'dart:developer';
+
+class FiltersHome extends StatefulWidget {
   const FiltersHome({Key? key}) : super(key: key);
+
+  @override
+  State<FiltersHome> createState() => _FiltersHomeState();
+}
+
+class _FiltersHomeState extends State<FiltersHome> {
+  bool isSwitched = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(context, 'Filter'),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.popUntil(context, ModalRoute.withName('/'));
-          },
-          child: const Text('Go home!'),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.popUntil(context, ModalRoute.withName('/'));
+                },
+                child: const Text('Go home!'),
+              ),
+            ),
+            Wrap(children: [
+              SizedBox(
+                width: 400,
+                child: Column(children: [
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/manualsHome');
+                        },
+                        child: const Text('Bluetooth settings'),
+                      ),
+                      Row(
+                        children: [
+                          const Text('Enable Bluetooth'),
+                          Switch(
+                            value: isSwitched,
+                            onChanged: (value) {
+                              setState(() {
+                                isSwitched = value;
+                                log(isSwitched.toString());
+                              });
+                            },
+                            activeTrackColor: AppTheme.colors.lightBlue,
+                            activeColor: AppTheme.colors.darkBlue,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Text('Paired Devices'),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Connect'),
+                      ),
+                    ],
+                  )
+                ]),
+              ),
+              SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/manualsHome');
+                    },
+                    child: const Text(
+                      'Manual',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )),
+            ]),
+          ],
         ),
       ),
     );
--- lib/pages/manuals.dart
+++ lib/pages/manuals.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:passage_flutter/theme/app_theme.dart';
 import '../components/custom_app_bar.dart';
 
-class ManualsHome extends StatelessWidget {
+import 'dart:developer';
+
+class ManualsHome extends StatefulWidget {
   const ManualsHome({Key? key}) : super(key: key);
 
+  @override
+  State<ManualsHome> createState() => _ManualsHomeState();
+}
+
+class _ManualsHomeState extends State<ManualsHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(context, 'Manuals'),
-      body: Stack(children: <Widget>[
-        Positioned(
-          top: 10,
-          left: 10,
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.popUntil(context, ModalRoute.withName('/'));
-            },
-            child: const Text('Go home!'),
-          ),
-        ),
-        Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.popUntil(context, ModalRoute.withName('/'));
-            },
-            child: const Text('Go home!'),
-          ),
+      body: Column(children: [
+        Wrap(
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.popUntil(context, ModalRoute.withName('/'));
+              },
+              child: const Text('Go Home'),
+            ),
+          ],
         ),
       ]),
     );
